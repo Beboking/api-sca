c8ba6e6d326b675486cc44f67d5fd82163c40f1a
sergiopacheco <sergio.pacheco@triplecyber.com.do>
2025-06-19 01:48:20 -0400

    test

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const { getGamesByPlayer } = require("./pgnService");
 
 const app = express();
-const PORT = 3000;
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
 
 app.get("/games", (req, res) => {
   const player = req.query.player;
